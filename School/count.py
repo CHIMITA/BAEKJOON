6244513452cfb6708a410435df65b323b5567b42
CHIMITA <smile0375-@naver.com>
2023-03-30 05:50:27 +0900

    reset

--- School/count.py
+++
@@ -1,7 +0,0 @@
-def hello(count):
-    if count ==0:
-        return
-    print('Hello, World!', count)
-    count -= 1
-    hello(count)
-  hello(5)
--- School/number.py
+++
@@ -1,8 +0,0 @@
-a, b = map(int, input('숫자를 두번 입력하세용 : ').split())
-
-if a + b >= 100:
-    print('100 이상입니당')
-else:
-    print('100 이하입니당')
-
-
